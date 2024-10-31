40ba343f4389607525f1b8a4ab204cfbcc4b3a7e
an-lime <andrej.gorbunov.2006@mail.ru>
2024-10-31 11:00:09 +0300

    commit2

--- src/main/java/org/example/courseproject/StartApplication.java
+++ src/main/java/org/example/courseproject/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("testGit!");
+        stage.setTitle("testGit2!");
         stage.setScene(scene);
         stage.show();
     }
